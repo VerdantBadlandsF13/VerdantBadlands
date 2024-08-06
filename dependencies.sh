7e422f9c3351a317976f60a53b45f414371f5a90
out-of-phaze <out.of.p.haze@proton.me>
2024-08-05 22:31:37 -0400

    Downgrade Node and upgrade Yarn and webpack

--- dependencies.sh
+++ dependencies.sh
@@ -11,8 +11,8 @@ export BYOND_MINOR=1627
 export RUST_G_VERSION=0.5.0
 
 #node version
-export NODE_VERSION=16
-export NODE_VERSION_PRECISE=16.20.2
+export NODE_VERSION=14
+export NODE_VERSION_PRECISE=14.16.1
 
 # SpacemanDMM git tag
 export SPACEMAN_DMM_VERSION=suite-1.9
